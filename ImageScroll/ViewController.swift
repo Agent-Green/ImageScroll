1078bfe9fe2ee669d66e86871fbf81c5c3ce308a
agent green <a.2000.g1@yandex.ru>
2024-06-08 17:30:53 +0300

    Initial Commit

---
+++ ImageScroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageScroll
+//
+//  Created by Алиса  Грищенкова on 08.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
